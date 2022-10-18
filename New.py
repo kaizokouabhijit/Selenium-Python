5ea787ec6811f0d0abb80bffcc2ac640dbc090ad
Abhijit Shubham <31901950+kaizokouabhijit@users.noreply.github.com>
2022-10-18 10:27:21 +0530

    Update New.py

--- New.py
+++ New.py
@@ -1,12 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 
 driver = webdriver.Chrome('./Drivers/chromedriver')
 driver.get("https://www.python.org")
 
+search_bar = driver.find_element(By.NAME, 'q')
+search_bar.clear()
+search_bar.send_keys('getting started with python')
+search_bar.send_keys(Keys.RETURN)
 
 
-print('The current url is : {}'.format(driver.current_url))
 
-# driver.close()
+
+print('\nThe current url is : {}\n'.format(driver.current_url))
+
+driver.close()
